192a11c2c85152509035172f99d700f787fc5886
ABEL\abelm <abelmanuellugtujr1996@gmail.com>
2022-08-03 11:17:22 +0800

    calculator

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -1,10 +1,15 @@
 import 'package:calculator/calculator.dart' as calculator;
 
 void main(List<String> arguments) {
-  try {
-    print(
-        'Hello world: ${calculator.calculate()}! \nArgs sum: ${arguments.fold<int>(0, ((previousValue, element) => previousValue + int.parse(element)))}');
-  } catch (e) {
-    print("Error: At least one argument is not an integer");
-  }
+  // try {
+  //   print(
+  //       'Hello world: ${calculator.calculate()}! \nArgs sum: ${arguments.fold<int>(0, ((previousValue, element) => previousValue + int.parse(element)))}');
+  // } catch (e) {
+  //   print("Error: At least one argument is not an integer");
+  // }
+
+  print('Add: ${calculator.calculateAdd().toString()}');
+  print('Subtract: ${calculator.calculateSubtract().toString()}');
+  print('Multiple: ${calculator.calculateMultiply().toString()}');
+  print('Divide: ${calculator.calculateDivide().toString()}');
 }
--- lib/src/calculate.dart
+++ lib/src/calculate.dart
@@ -1,3 +1,21 @@
+import 'package:basic_arithmetic/calculator.dart';
+
 int calculate() {
   return 6 * 7;
+}
+
+int calculateAdd() {
+  return add(6, 8);
+}
+
+int calculateSubtract() {
+  return subtract(6, 8);
+}
+
+int calculateMultiply() {
+  return multiply(6, 8);
+}
+
+double calculateDivide() {
+  return divide(6, 8);
 }
